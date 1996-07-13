c724bd59cf79bd6dc6afe12239239ef3801d3e0e
Miles Bader <miles@gnu.ai.mit.edu>
1996-07-13 04:36:06 +0000

    (env_init): Use localhost.

--- telnet/commands.c
+++ telnet/commands.c
@@ -1712,15 +1712,17 @@ env_init()
 	if ((ep = env_find("DISPLAY"))
 	    && ((*ep->value == ':')
 	        || (strncmp((char *)ep->value, "unix:", 5) == 0))) {
-		char hbuf[256+1];
+		extern char *localhost ();
+		char *hostname = localhost ();
 		char *cp2 = strchr((char *)ep->value, ':');
 
-		gethostname(hbuf, 256);
-		hbuf[256] = '\0';
-		cp = (char *)malloc(strlen(hbuf) + strlen(cp2) + 1);
-		sprintf((char *)cp, "%s%s", hbuf, cp2);
+		cp = (char *)malloc(strlen(hostname) + strlen(cp2) + 1);
+		sprintf((char *)cp, "%s%s", localhost, cp2);
+
 		free(ep->value);
 		ep->value = (unsigned char *)cp;
+
+		free (hostname);
 	}
 	/*
 	 * If USER is not defined, but LOGNAME is, then add
