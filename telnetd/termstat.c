ef703ab100713268c8f22633248eefa6499340fe
Miles Bader <miles@gnu.ai.mit.edu>
1997-01-21 02:09:07 +0000

    (localstat): Evaluate tty_istrapsig() not tty_istrapsig.

--- telnetd/termstat.c
+++ telnetd/termstat.c
@@ -445,7 +445,7 @@ clientstat(code, parm1, parm2)
 				useeditmode = 0;
 				if (tty_isediting())
 					useeditmode |= MODE_EDIT;
-				if (tty_istrapsig)
+				if (tty_istrapsig())
 					useeditmode |= MODE_TRAPSIG;
 				if (tty_issofttab())
 					useeditmode |= MODE_SOFT_TAB;
