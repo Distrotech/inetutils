0c33f5e43df65e33f4ebd5fa9fda121ad90ab591
Miles Bader <miles@gnu.ai.mit.edu>
1997-02-25 23:26:35 +0000

    From David J MacKenzie <djm@va.pubnix.com>: (validate_access):   Truncate files opened for writing.

--- tftpd/tftpd.c
+++ tftpd/tftpd.c
@@ -424,7 +424,7 @@ validate_access(filep, mode)
 			return (err);
 		*filep = filename = pathname;
 	}
-	fd = open(filename, mode == RRQ ? O_RDONLY : O_WRONLY);
+	fd = open(filename, mode == RRQ ? O_RDONLY : (O_WRONLY | O_TRUNC));
 	if (fd < 0)
 		return (errno + 100);
 	file = fdopen(fd, (mode == RRQ)? "r":"w");
