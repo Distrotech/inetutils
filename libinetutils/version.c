1ae8163e3559c9748b5175e0967a402676ab2d67
Miles Bader <miles@gnu.ai.mit.edu>
1997-09-15 19:10:02 +0000

    (inetutils_version):   Change to `1.3.1a'.

--- libinetutils/version.c
+++ libinetutils/version.c
@@ -4,5 +4,5 @@
 
 #include "version.h"
 const char *inetutils_package = "GNU inetutils";
-const char *inetutils_version = "1.3.2";
+const char *inetutils_version = "1.3.1a";
 const char *inetutils_bugaddr = "<bug-inetutils@gnu.ai.mit.edu>";
