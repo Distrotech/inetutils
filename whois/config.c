84bdb202a38c714996122b8e164d9423cec6e665
Miles Bader <miles@gnu.ai.mit.edu>
1998-04-11 01:13:21 +0000

    add

---
+++ whois/config.c
@@ -0,0 +1,10 @@
+char *whois_servers =
+"# This is the configuration file for the GNU whois client; it lists the\n"
+"# pattern for a whois query on the left, and the whois server on the right.\n"
+"# Comments start with # characters and go to the end of the line.\n"
+"\n"
+"*-arin		whois.arin.net\n"
+"[0-9]*		whois.arin.net\n"
+"*.mil		nic.ddn.net\n"
+"*		whois.internic.net\n"
+;
