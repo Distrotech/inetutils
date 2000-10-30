0d0ca60f9d5b0666f9683a895b73c04d2c322c81
Marcus Brinkmann <marcus@gnu.org>
2000-10-30 23:20:37 +0000

    2000-10-30  Marcus Brinkmann  <marcus@gnu.org>
    	* main.c: Convert to GNU coding standards.
    
    	* main.c: Remove COPYRIGHT (it's in the header) and
    	"#if 0"-out SCCSID.
    	(ohelp): Removed.
    	(try_help): Removed.
    	(usage): Incorporate functionality of ohelp and try_help,
    	and accept new argument ERR.
    
    	* main.c (getcmd): Add parenthese around assignment used as truth
    	value to silence gcc warning.
    	(makeargv): Likewise.

--- ftp/main.c
+++ ftp/main.c
@@ -27,15 +27,9 @@
  * SUCH DAMAGE.
  */
 
-#ifndef lint
-static char copyright[] =
-"@(#) Copyright (c) 1985, 1989, 1993, 1994\n\
-	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
-
-#ifndef lint
+#if 0
 static char sccsid[] = "@(#)main.c	8.6 (Berkeley) 10/9/94";
-#endif /* not lint */
+#endif
 
 /*
  * FTP User Program -- Command Interface.
@@ -66,21 +60,24 @@ static char sccsid[] = "@(#)main.c	8.6 (Berkeley) 10/9/94";
 #define FTP_EXTERN
 #include "ftp_var.h"
 
-#define USAGE "Usage: %s [OPTION...] [HOST [PORT]]\n"
-
 /* basename (argv[0]).  NetBSD, linux, & gnu libc all define it.  */
 extern char *__progname;
 
 #define DEFAULT_PROMPT "ftp> "
 static char *prompt = 0;
 
-/* Print a help message describing all options to STDOUT and exit with a
-   status of 0.  */
 static void
-ohelp ()
+usage (int err)
 {
-  fprintf (stdout, USAGE, __progname);
-  puts ("Remote file transfer\n\n\
+  if (err != 0)
+    {
+      fprintf (stderr, "Usage: %s [OPTION...] [HOST [PORT]]\n", __progname);
+      fprintf (stderr, "Try `%s --help' for more information.\n", __progname);
+    }
+  else
+    {
+      fprintf (stdout, "Usage: %s [OPTION...] [HOST [PORT]]\n", __progname);
+      puts ("Remote file transfer.\n\n\
   -d, --debug                Turn on debugging mode\n\
   -g, --no-glob              Turn off file name globbing\n\
   -i, --no-prompt            Don't prompt during multiple-file transfers\n\
@@ -90,28 +87,14 @@ ohelp ()
                              even if not on a tty\n\
   -v, --verbose              Be verbose\n\
       --help                 Give this help list\n\
-      --version              Print program version");
-  fprintf (stdout, "\nSubmit bug reports to %s.\n", inetutils_bugaddr);
-  exit (0);
+  -V, --version              Print program version");
+
+      fprintf (stdout, "\nSubmit bug reports to %s.\n", inetutils_bugaddr);
+    }
+  exit (err);
 }
 
-/* Print a message saying to use --help to STDERR, and exit with a status of
-   1.  */
-static void
-try_help ()
-{
-  fprintf (stderr, "Try `%s --help' for more information.\n", __progname);
-  exit (1);
-}
-
-/* Print a usage message to STDERR and exit with a status of 1.  */
-static void
-usage ()
-{
-  fprintf (stderr, USAGE, __progname);
-  try_help ();
-}
-
+static const char *short_options = "dginp::tv";
 static struct option long_options[] =
 {
   { "trace", no_argument, 0, 't' },
@@ -127,167 +110,170 @@ static struct option long_options[] =
 };
 
 int
-main(argc, argv)
-	int argc;
-	char *argv[];
+main (int argc, char *argv[])
 {
-	int ch, top;
-	struct passwd *pw = NULL;
-	char *cp;
+  int ch, top;
+  struct passwd *pw = NULL;
+  char *cp;
 
 #ifndef HAVE___PROGNAME
-	extern char *__progname;
-	__progname = argv[0];
+  __progname = argv[0];
 #endif
 
-	sp = getservbyname("ftp", "tcp");
-	if (sp == 0)
-		errx(1, "ftp/tcp: unknown service");
-	doglob = 1;
-	interactive = 1;
-	autologin = 1;
+  sp = getservbyname ("ftp", "tcp");
+  if (sp == 0)
+    errx (1, "ftp/tcp: unknown service");
+  doglob = 1;
+  interactive = 1;
+  autologin = 1;
 
-	while ((ch = getopt_long (argc, argv, "dginp::tv", long_options, 0))
-	       != EOF)
+  while ((ch = getopt_long (argc, argv, short_options, long_options, 0))
+	 != EOF)
+    {
+      switch (ch) {
+      case 'd': /* Enable ebug mode.  */
+	options |= SO_DEBUG;
+	debug++;
+	break;
+
+      case 'g': /* No glob.  */
+	doglob = 0;
+	break;
+
+      case 'i': /* No prompt.  */
+	interactive = 0;
+	break;
+
+      case 'n': /* No automatic login.  */
+	autologin = 0;
+	break;
+
+      case 't': /* Enable packet tracing.  */
+	trace++;
+	break;
+
+      case 'v': /* Verbose.  */
+	verbose++;
+	break;
+
+      case 'p': /* Print command line prompt.  */
+	prompt = optarg ? optarg : DEFAULT_PROMPT;
+	break;
+
+      case '&': /* Usage.  */
+	usage (0);
+	/* Not reached.  */
+
+      case 'V': /* Version.  */
+	printf ("ftp (%s) %s\n", inetutils_package, inetutils_version);
+	exit (0);
+
+      case '?':
+      default:
+	usage (1);
+	/* Not reached.  */
+      }
+    }
+
+  argc -= optind;
+  argv += optind;
+
+  fromatty = isatty (fileno (stdin));
+  if (fromatty)
+    {
+      verbose++;
+      if (! prompt)
+	prompt = DEFAULT_PROMPT;
+    }
+
+  cpend = 0;	/* no pending replies */
+  proxy = 0;	/* proxy not active */
+  passivemode = 0; /* passive mode not active */
+  crflag = 1;	/* strip c.r. on ascii gets */
+  sendport = -1;	/* not using ports */
+  /*
+   * Set up the home directory in case we're globbing.
+   */
+  cp = getlogin ();
+  if (cp != NULL)
+    pw = getpwnam (cp);
+  if (pw == NULL)
+    pw = getpwuid(getuid());
+  if (pw != NULL)
+    {
+      char *buf = malloc (strlen (pw->pw_dir) + 1);
+      if (buf)
 	{
-		switch (ch) {
-		case 'd':
-			options |= SO_DEBUG;
-			debug++;
-			break;
-
-		case 'g':
-			doglob = 0;
-			break;
-
-		case 'i':
-			interactive = 0;
-			break;
-
-		case 'n':
-			autologin = 0;
-			break;
-
-		case 't':
-			trace++;
-			break;
-
-		case 'v':
-			verbose++;
-			break;
-
-                case 'p':
-		        prompt = optarg ? optarg : DEFAULT_PROMPT;
-			break;
-
-		case '&':
-			ohelp ();
-		case 'V':
-			printf ("ftp (%s) %s\n",
-				inetutils_package, inetutils_version);
-			exit (0);
-
-		case '?':
-			try_help ();
-
-		default:
-			usage ();
-		}
+	  strcpy (buf, pw->pw_dir);
+	  home = buf;
 	}
-	argc -= optind;
-	argv += optind;
+    }
+  if (argc > 0)
+    {
+      char *xargv[5];
 
-	fromatty = isatty(fileno(stdin));
-	if (fromatty) {
-		verbose++;
-		if (! prompt)
-			prompt = DEFAULT_PROMPT;
-	}
-
-	cpend = 0;	/* no pending replies */
-	proxy = 0;	/* proxy not active */
-	passivemode = 0; /* passive mode not active */
-	crflag = 1;	/* strip c.r. on ascii gets */
-	sendport = -1;	/* not using ports */
-	/*
-	 * Set up the home directory in case we're globbing.
-	 */
-	cp = getlogin();
-	if (cp != NULL) {
-		pw = getpwnam(cp);
-	}
-	if (pw == NULL)
-		pw = getpwuid(getuid());
-	if (pw != NULL) {
-		char *buf = malloc (strlen (pw->pw_dir) + 1);
-		if (buf) {
-			strcpy(buf, pw->pw_dir);
-			home = buf;
-		}
-	}
-	if (argc > 0) {
-		char *xargv[5];
-		extern char *__progname;
-
-		if (setjmp(toplevel))
-			exit(0);
-		(void) signal(SIGINT, intr);
-		(void) signal(SIGPIPE, lostpeer);
-		xargv[0] = __progname;
-		xargv[1] = argv[0];
-		xargv[2] = argv[1];
-		xargv[3] = argv[2];
-		xargv[4] = NULL;
-		setpeer(argc+1, xargv);
-	}
-	top = setjmp(toplevel) == 0;
-	if (top) {
-		(void) signal(SIGINT, intr);
-		(void) signal(SIGPIPE, lostpeer);
-	}
-	for (;;) {
-		cmdscanner(top);
-		top = 1;
-	}
+      if (setjmp (toplevel))
+	exit (0);
+      (void) signal(SIGINT, intr);
+      (void) signal(SIGPIPE, lostpeer);
+      xargv[0] = __progname;
+      xargv[1] = argv[0];
+      xargv[2] = argv[1];
+      xargv[3] = argv[2];
+      xargv[4] = NULL;
+      setpeer (argc+1, xargv);
+    }
+  top = setjmp (toplevel) == 0;
+  if (top)
+    {
+      (void) signal (SIGINT, intr);
+      (void) signal (SIGPIPE, lostpeer);
+    }
+  for (;;)
+    {
+      cmdscanner(top);
+      top = 1;
+    }
 }
 
 void
-intr(sig)
-  int sig;
+intr (int sig)
 {
-
-	longjmp(toplevel, 1);
+  longjmp (toplevel, 1);
 }
 
 void
-lostpeer(sig)
-  int sig;
+lostpeer (int sig)
 {
-
-	if (connected) {
-		if (cout != NULL) {
-			(void) shutdown(fileno(cout), 1+1);
-			(void) fclose(cout);
-			cout = NULL;
-		}
-		if (data >= 0) {
-			(void) shutdown(data, 1+1);
-			(void) close(data);
-			data = -1;
-		}
-		connected = 0;
+  if (connected)
+    {
+      if (cout != NULL)
+	{
+	  (void) shutdown (fileno (cout), 1+1);
+	  (void) fclose (cout);
+	  cout = NULL;
 	}
-	pswitch(1);
-	if (connected) {
-		if (cout != NULL) {
-			(void) shutdown(fileno(cout), 1+1);
-			(void) fclose(cout);
-			cout = NULL;
-		}
-		connected = 0;
+      if (data >= 0)
+	{
+	  (void) shutdown (data, 1+1);
+	  (void) close (data);
+	  data = -1;
 	}
-	proxflag = 0;
-	pswitch(0);
+      connected = 0;
+    }
+  pswitch (1);
+  if (connected)
+    {
+      if (cout != NULL)
+	{
+	  (void) shutdown (fileno(cout), 1+1);
+	  (void) fclose (cout);
+	  cout = NULL;
+	}
+      connected = 0;
+    }
+  proxflag = 0;
+  pswitch (0);
 }
 
 /*
@@ -313,105 +299,116 @@ tail(filename)
  * Command parser.
  */
 void
-cmdscanner(top)
-	int top;
+cmdscanner (int top)
 {
-	struct cmd *c;
-	int l;
+  struct cmd *c;
+  int l;
 
-	if (!top)
-		(void) putchar('\n');
-	for (;;) {
+  if (!top)
+    (void) putchar ('\n');
+  for (;;)
+    {
 
 #if HAVE_LIBREADLINE
-		if (line) {
-			free(line);
-			line = 0;
-		}
-		line = readline (prompt);
-		if (!line)
-			quit (0, 0);
-		if (line && *line)
-			add_history (line);
-		l = strlen(line);
-		if (l == 0)
-			break;
-#else
-		if (prompt) {
-			printf ("%s",prompt);
-			fflush(stdout);
-		}
-
-		if (fgets(line, sizeof line, stdin) == NULL)
-			quit(0, 0);
-		l = strlen(line);
-		if (l == 0)
-			break;
-		if (line[--l] == '\n') {
-			if (l == 0)
-				break;
-			line[l] = '\0';
-		} else if (l == sizeof(line) - 2) {
-			printf("sorry, input line too long\n");
-			while ((l = getchar()) != '\n' && l != EOF)
-				/* void */;
-			break;
-		} /* else it was a line without a newline */
-#endif
-		makeargv();
-		if (margc == 0) {
-			continue;
-		}
-		c = getcmd(margv[0]);
-		if (c == (struct cmd *)-1) {
-			printf("?Ambiguous command\n");
-			continue;
-		}
-		if (c == 0) {
-			printf("?Invalid command\n");
-			continue;
-		}
-		if (c->c_conn && !connected) {
-			printf("Not connected.\n");
-			continue;
-		}
-		(*c->c_handler)(margc, margv);
-		if (bell && c->c_bell)
-			(void) putchar('\007');
-		if (c->c_handler != help)
-			break;
+      if (line)
+	{
+	  free (line);
+	  line = 0;
 	}
-	(void) signal(SIGINT, intr);
-	(void) signal(SIGPIPE, lostpeer);
+      line = readline (prompt);
+      if (!line)
+	quit (0, 0);
+      if (line && *line)
+	add_history (line);
+      l = strlen (line);
+      if (l == 0)
+	break;
+#else
+      if (prompt)
+	{
+	  printf ("%s",prompt);
+	  fflush(stdout);
+	}
+
+      if (fgets (line, sizeof line, stdin) == NULL)
+	quit(0, 0);
+      l = strlen (line);
+      if (l == 0)
+	break;
+      if (line[--l] == '\n')
+	{
+	  if (l == 0)
+	    break;
+	  line[l] = '\0';
+	}
+      else if (l == sizeof (line) - 2)
+	{
+	  printf ("sorry, input line too long\n");
+	  while ((l = getchar ()) != '\n' && l != EOF)
+				/* void */;
+	  break;
+	} /* else it was a line without a newline */
+#endif
+      makeargv ();
+      if (margc == 0)
+	continue;
+
+      c = getcmd (margv[0]);
+      if (c == (struct cmd *) -1)
+	{
+	  printf ("?Ambiguous command\n");
+	  continue;
+	}
+      if (c == 0)
+	{
+	  printf ("?Invalid command\n");
+	  continue;
+	}
+      if (c->c_conn && !connected)
+	{
+	  printf ("Not connected.\n");
+	  continue;
+	}
+      (*c->c_handler) (margc, margv);
+      if (bell && c->c_bell)
+	(void) putchar('\007');
+      if (c->c_handler != help)
+	break;
+    }
+  (void) signal (SIGINT, intr);
+  (void) signal (SIGPIPE, lostpeer);
 }
 
 struct cmd *
-getcmd(name)
-	char *name;
+getcmd (char *name)
 {
-	char *p, *q;
-	struct cmd *c, *found;
-	int nmatches, longest;
+  char *p, *q;
+  struct cmd *c, *found;
+  int nmatches, longest;
 
-	longest = 0;
-	nmatches = 0;
-	found = 0;
-	for (c = cmdtab; p = c->c_name; c++) {
-		for (q = name; *q == *p++; q++)
-			if (*q == 0)		/* exact match? */
-				return (c);
-		if (!*q) {			/* the name was a prefix */
-			if (q - name > longest) {
-				longest = q - name;
-				nmatches = 1;
-				found = c;
-			} else if (q - name == longest)
-				nmatches++;
-		}
+  longest = 0;
+  nmatches = 0;
+  found = 0;
+  for (c = cmdtab; (p = c->c_name); c++)
+    {
+      for (q = name; *q == *p++; q++)
+	if (*q == 0)		/* exact match? */
+	  return (c);
+      if (!*q)
+	{			/* the name was a prefix */
+	  if (q - name > longest)
+	    {
+	      longest = q - name;
+	      nmatches = 1;
+	      found = c;
+	    }
+	  else if (q - name == longest)
+	    nmatches++;
 	}
-	if (nmatches > 1)
-		return ((struct cmd *)-1);
-	return (found);
+    }
+  if (nmatches > 1)
+    return ((struct cmd *)-1);
+  return (found);
 }
 
 /*
@@ -421,17 +418,17 @@ getcmd(name)
 int slrflag;
 
 void
-makeargv()
+makeargv ()
 {
-	char **argp;
+  char **argp;
 
-	margc = 0;
-	argp = margv;
-	stringbase = line;		/* scan from first of buffer */
-	argbase = argbuf;		/* store from first of buffer */
-	slrflag = 0;
-	while (*argp++ = slurpstring())
-		margc++;
+  margc = 0;
+  argp = margv;
+  stringbase = line;		/* scan from first of buffer */
+  argbase = argbuf;		/* store from first of buffer */
+  slrflag = 0;
+  while ((*argp++ = slurpstring ()))
+    margc++;
 }
 
 /*
@@ -440,121 +437,129 @@ makeargv()
  * handle quoting and strings
  */
 char *
-slurpstring()
+slurpstring ()
 {
-	int got_one = 0;
-	char *sb = stringbase;
-	char *ap = argbase;
-	char *tmp = argbase;		/* will return this if token found */
+  int got_one = 0;
+  char *sb = stringbase;
+  char *ap = argbase;
+  char *tmp = argbase;		/* will return this if token found */
 
-	if (*sb == '!' || *sb == '$') {	/* recognize ! as a token for shell */
-		switch (slrflag) {	/* and $ as token for macro invoke */
-			case 0:
-				slrflag++;
-				stringbase++;
-				return ((*sb == '!') ? "!" : "$");
-				/* NOTREACHED */
-			case 1:
-				slrflag++;
-				altarg = stringbase;
-				break;
-			default:
-				break;
-		}
-	}
+  if (*sb == '!' || *sb == '$') {	/* recognize ! as a token for shell */
+    switch (slrflag)	/* and $ as token for macro invoke */
+      {
+      case 0:
+	slrflag++;
+	stringbase++;
+	return ((*sb == '!') ? "!" : "$");
+	/* NOTREACHED */
 
-S0:
-	switch (*sb) {
+      case 1:
+	slrflag++;
+	altarg = stringbase;
+	break;
+      
+      default:
+	break;
+      }
+  }
 
-	case '\0':
-		goto OUT;
+ S0:
+  switch (*sb)
+    {
+    case '\0':
+      goto OUT;
 
-	case ' ':
-	case '\t':
-		sb++; goto S0;
+    case ' ':
+    case '\t':
+      sb++; goto S0;
 
-	default:
-		switch (slrflag) {
-			case 0:
-				slrflag++;
-				break;
-			case 1:
-				slrflag++;
-				altarg = sb;
-				break;
-			default:
-				break;
-		}
-		goto S1;
-	}
+    default:
+      switch (slrflag)
+	{
+	case 0:
+	  slrflag++;
+	  break;
 
-S1:
-	switch (*sb) {
-
-	case ' ':
-	case '\t':
-	case '\0':
-		goto OUT;	/* end of token */
-
-	case '\\':
-		sb++; goto S2;	/* slurp next character */
-
-	case '"':
-		sb++; goto S3;	/* slurp quoted string */
+	case 1:
+	  slrflag++;
+	  altarg = sb;
+	  break;
 
 	default:
-		*ap++ = *sb++;	/* add character to token */
-		got_one = 1;
-		goto S1;
+	  break;
 	}
+      goto S1;
+    }
 
-S2:
-	switch (*sb) {
+ S1:
+  switch (*sb)
+    {
+    case ' ':
+    case '\t':
+    case '\0':
+      goto OUT;	/* end of token */
 
-	case '\0':
-		goto OUT;
+    case '\\':
+      sb++; goto S2;	/* slurp next character */
 
-	default:
-		*ap++ = *sb++;
-		got_one = 1;
-		goto S1;
-	}
+    case '"':
+      sb++; goto S3;	/* slurp quoted string */
 
-S3:
-	switch (*sb) {
+    default:
+      *ap++ = *sb++;	/* add character to token */
+      got_one = 1;
+      goto S1;
+    }
 
-	case '\0':
-		goto OUT;
+ S2:
+  switch (*sb)
+    {
+    case '\0':
+      goto OUT;
+      
+    default:
+      *ap++ = *sb++;
+      got_one = 1;
+      goto S1;
+    }
 
-	case '"':
-		sb++; goto S1;
+ S3:
+  switch (*sb)
+    {
+    case '\0':
+      goto OUT;
+      
+    case '"':
+      sb++; goto S1;
+      
+    default:
+      *ap++ = *sb++;
+      got_one = 1;
+      goto S3;
+    }
 
-	default:
-		*ap++ = *sb++;
-		got_one = 1;
-		goto S3;
-	}
+ OUT:
+  if (got_one)
+    *ap++ = '\0';
+  argbase = ap;			/* update storage pointer */
+  stringbase = sb;		/* update scan pointer */
+  if (got_one)
+    return (tmp);
+  switch (slrflag)
+    {
+    case 0:
+      slrflag++;
+      break;
 
-OUT:
-	if (got_one)
-		*ap++ = '\0';
-	argbase = ap;			/* update storage pointer */
-	stringbase = sb;		/* update scan pointer */
-	if (got_one) {
-		return (tmp);
-	}
-	switch (slrflag) {
-		case 0:
-			slrflag++;
-			break;
-		case 1:
-			slrflag++;
-			altarg = (char *) 0;
-			break;
-		default:
-			break;
-	}
-	return ((char *)0);
+    case 1:
+      slrflag++;
+      altarg = (char *) 0;
+      break;
+
+    default:
+      break;
+    }
+  return ((char *) 0);
 }
 
 #define HELPINDENT ((int) sizeof ("directory"))
@@ -564,62 +569,69 @@ OUT:
  * Call each command handler with argc == 0 and argv[0] == name.
  */
 void
-help(argc, argv)
-	int argc;
-	char *argv[];
+help (int argc, char *argv[])
 {
-	struct cmd *c;
+  struct cmd *c;
 
-	if (argc == 1) {
-		int i, j, w, k;
-		int columns, width = 0, lines;
+  if (argc == 1)
+    {
+      int i, j, w, k;
+      int columns, width = 0, lines;
 
-		printf("Commands may be abbreviated.  Commands are:\n\n");
-		for (c = cmdtab; c < &cmdtab[NCMDS]; c++) {
-			int len = strlen(c->c_name);
+      printf ("Commands may be abbreviated.  Commands are:\n\n");
+      for (c = cmdtab; c < &cmdtab[NCMDS]; c++)
+	{
+	  int len = strlen (c->c_name);
 
-			if (len > width)
-				width = len;
-		}
-		width = (width + 8) &~ 7;
-		columns = 80 / width;
-		if (columns == 0)
-			columns = 1;
-		lines = (NCMDS + columns - 1) / columns;
-		for (i = 0; i < lines; i++) {
-			for (j = 0; j < columns; j++) {
-				c = cmdtab + j * lines + i;
-				if (c->c_name && (!proxy || c->c_proxy)) {
-					printf("%s", c->c_name);
-				}
-				else if (c->c_name) {
-					for (k=0; k < strlen(c->c_name); k++) {
-						(void) putchar(' ');
-					}
-				}
-				if (c + lines >= &cmdtab[NCMDS]) {
-					printf("\n");
-					break;
-				}
-				w = strlen(c->c_name);
-				while (w < width) {
-					w = (w + 8) &~ 7;
-					(void) putchar('\t');
-				}
-			}
-		}
-		return;
+	  if (len > width)
+	    width = len;
 	}
-	while (--argc > 0) {
-		char *arg;
-		arg = *++argv;
-		c = getcmd(arg);
-		if (c == (struct cmd *)-1)
-			printf("?Ambiguous help command %s\n", arg);
-		else if (c == (struct cmd *)0)
-			printf("?Invalid help command %s\n", arg);
-		else
-			printf("%-*s\t%s\n", HELPINDENT,
-				c->c_name, c->c_help);
+      width = (width + 8) &~ 7;
+      columns = 80 / width;
+      if (columns == 0)
+	columns = 1;
+      lines = (NCMDS + columns - 1) / columns;
+      for (i = 0; i < lines; i++)
+	{
+	  for (j = 0; j < columns; j++)
+	    {
+	      c = cmdtab + j * lines + i;
+	      if (c->c_name && (!proxy || c->c_proxy))
+		{
+		  printf ("%s", c->c_name);
+		}
+	      else if (c->c_name)
+		{
+		  for (k=0; k < strlen (c->c_name); k++)
+		    {
+		      (void) putchar (' ');
+		    }
+		}
+	      if (c + lines >= &cmdtab[NCMDS])
+		{
+		  printf ("\n");
+		  break;
+		}
+	      w = strlen (c->c_name);
+	      while (w < width) {
+		w = (w + 8) &~ 7;
+		(void) putchar ('\t');
+	      }
+	    }
 	}
+      return;
+    }
+  while (--argc > 0)
+    {
+      char *arg;
+      arg = *++argv;
+      c = getcmd(arg);
+      if (c == (struct cmd *)-1)
+	printf("?Ambiguous help command %s\n", arg);
+      else if (c == (struct cmd *)0)
+	printf("?Invalid help command %s\n", arg);
+      else
+	printf("%-*s\t%s\n", HELPINDENT,
+	       c->c_name, c->c_help);
+    }
 }
