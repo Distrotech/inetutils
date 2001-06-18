d53b2bf22b154c2521111493bab15b93a64a9075
Sergey Poznyakoff <gray@Mirddin.farlep.net>
2001-06-18 15:54:35 +0000

    reflect changes to libping.c

--- libicmp/ping.h
+++ libicmp/ping.h
@@ -22,28 +22,28 @@ typedef int (*ping_efp) __P((int code,
 			     void *closure,
 			     struct sockaddr_in *dest, 
 			     struct sockaddr_in *from,
-			     u_char *data,
-			     int datalen));
+			     struct ip *ip, icmphdr_t *icmp, int datalen));
+
 
 struct ping_data
 {
   int    ping_fd;        /* Raw socket descriptor */
+  int    ping_type;      /* Type of packets to send */
   int    ping_count;     /* Number of packets to send */
   int    ping_interval;  /* Number of seconds to wait between sending pkts */
   struct sockaddr_in ping_dest; /* whom to ping */
   char   *ping_hostname;     /* Printable hostname */
   int    ping_datalen;   /* Length of data */
-  u_char *ping_outpack;  /* Packet to send */
   int    ping_ident;     /* Our identifier */
                           
   ping_efp ping_event;   /* User-defined handler */
-  void *ping_closure;                /* User-defined data */
+  void *ping_closure;    /* User-defined data */
 
   /* Runtime info */
   int ping_cktab_size;
   char   *ping_cktab;
   
-  u_char *ping_inpack;   /* Received packet */
+  u_char *ping_buffer;   /* I/O buffer */
   struct sockaddr_in ping_from;
   int    ping_num_xmit;  /* Number of packets transmitted */
   int    ping_num_recv;  /* Number of packets received */
@@ -57,12 +57,7 @@ struct ping_data
 #define PING_INTERVAL 1
 #define	PING_CKTABSIZE 128
 
-#define	PING_HEADER_LEN	sizeof(struct timeval)
-#define	PING_DATALEN	(64 - PING_HEADER_LEN)	/* default data length */
-
-#define _PING_OUTBUFLEN(p) ((p)->ping_datalen + PING_HEADER_LEN)
-#define _PING_INBUFLEN(p) ((p)->ping_datalen + MAXIPLEN + MAXICMPLEN)
-#define _PING_TIMING(p) ((p)->ping_datalen >= PING_HEADER_LEN)
+#define _PING_BUFLEN(p) ((p)->ping_datalen + sizeof(icmphdr_t))
 
 #define _C_BIT(p,bit)    (p)->ping_cktab[(bit)>>3]  /* byte in ck array */
 #define _C_MASK(bit)     (1 << ((bit) & 0x07))
@@ -71,7 +66,8 @@ struct ping_data
 #define _PING_CLR(p,bit) (_C_BIT(p,bit) &= (~_C_MASK(bit)))
 #define _PING_TST(p,bit) (_C_BIT(p,bit) & _C_MASK(bit))
 
-PING *ping_init(int ident);
+PING *ping_init(int type, int ident);
+void ping_set_type(PING *p, int type);
 void ping_set_count(PING *ping, int count);
 void ping_set_sockopt(PING *ping, int opt, void *val, int valsize);
 void ping_set_interval(PING *ping, int interval);
@@ -79,3 +75,5 @@ void ping_set_packetsize(PING *ping, int size);
 int ping_set_dest(PING *ping, char *host);
 int ping_set_pattern(PING *p, int len, u_char *pat);
 void ping_set_event_handler(PING *ping, ping_efp fp, void *closure);
+int ping_set_data(PING *p, void *data, size_t off, size_t len);
+void ping_set_datalen(PING *p, size_t len);
