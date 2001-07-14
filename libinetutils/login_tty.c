7dd44d9be064d18efcffe4e3be253e7c6fbc5172
Sergey Poznyakoff <gray@Mirddin.farlep.net>
2001-07-14 14:50:19 +0000

    Rewritten from scratch. This version works on systems lacking TIOCSCTTY ioctl

--- libinetutils/login_tty.c
+++ libinetutils/login_tty.c
@@ -1,35 +1,22 @@
-/*-
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
+/* Copyright (C) 2001 Free Software Foundation, Inc.
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)login_tty.c	8.1 (Berkeley) 6/4/93";
-#endif /* LIBC_SCCS and not lint */
+   This file is part of GNU Inetutils.
+
+   GNU Inetutils is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   GNU Inetutils is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Inetutils; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA. */
+
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -39,38 +26,93 @@ static char sccsid[] = "@(#)login_tty.c	8.1 (Berkeley) 6/4/93";
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <syslog.h>
+#include <errno.h>
+#include <signal.h>
 
 int
-login_tty(int fd)
+login_tty(int ttyfd)
 {
+  int fd;
+  char *fdname;
+
 #ifdef HAVE_SETSID
-	(void) setsid();
+  setsid();
 #endif
+#ifdef HAVE_SETPGID
+  setpgid(0, 0);
+#endif
+	
+  /* First disconnect from the old controlling tty. */
+#ifdef TIOCNOTTY
+  fd = open("/dev/tty", O_RDWR|O_NOCTTY);
+  if (fd >= 0)
+    {
+      ioctl(fd, TIOCNOTTY, NULL);
+      close(fd);
+    }
+  else
+    syslog(LOG_WARNING, "NO CTTY");
+#endif /* TIOCNOTTY */
+  
+  /* Verify that we are successfully disconnected from the controlling tty. */
+  fd = open("/dev/tty", O_RDWR|O_NOCTTY);
+  if (fd >= 0)
+    {
+      syslog(LOG_WARNING, "Failed to disconnect from controlling tty.");
+      close(fd);
+    }
+  
+  /* Make it our controlling tty. */
 #ifdef TIOCSCTTY
-	if (ioctl(fd, TIOCSCTTY, (char *)NULL) == -1)
-		return (-1);
-#else
-	{
-	  /* This might work.  */
-	  char *fdname = ttyname (fd);
-	  int newfd;
-	  if (fdname)
-	    {
-	      if (fd != 0)
-		(void) close (0);
-	      if (fd != 1)
-		(void) close (1);
-	      if (fd != 2)
-		(void) close (2);
-	      newfd = open (fdname, O_RDWR);
-	      (void) close (newfd);
-	    }
-	}
+  ioctl(ttyfd, TIOCSCTTY, NULL);
+#endif /* TIOCSCTTY */
+
+  fdname = ttyname (ttyfd);
+  fd = open(fdname, O_RDWR);
+  if (fd < 0)
+    syslog(LOG_WARNING, "open %s: %s", fdname, strerror(errno));
+  else
+    close(fd);
+
+  /* Verify that we now have a controlling tty. */
+  fd = open("/dev/tty", O_WRONLY);
+  if (fd < 0)
+    {
+      syslog(LOG_WARNING, "open /dev/tty: %s", strerror(errno));
+      return 1;
+    }
+
+  close(fd);
+#if defined(HAVE_VHANGUP) && !defined(HAVE_REVOKE)
+  {
+    RETSIGTYPE (*sig)();
+    sig = signal(SIGHUP, SIG_IGN);
+    vhangup();
+    signal(SIGHUP, sig);
+  }
 #endif
-	(void) dup2(fd, 0);
-	(void) dup2(fd, 1);
-	(void) dup2(fd, 2);
-	if (fd > 2)
-		(void) close(fd);
-	return (0);
+  fd = open(fdname, O_RDWR);
+  if (fd == -1)
+    {
+      syslog(LOG_ERR, "can't reopen ctty %s: %s", fdname, strerror(errno));
+      return -1;
+    }
+	
+  close(ttyfd);
+
+  if (fd != 0)
+    close(0);
+  if (fd != 1)
+    close(1);
+  if (fd != 2)
+    close(2);
+
+  dup2(fd, 0);
+  dup2(fd, 1);
+  dup2(fd, 2);
+  if (fd > 2)
+    close(fd);
+  return 0;
 }
+
