7b99bd386436e75368d2cfef51b3450e422f5891
Alfred M. Szmidt <ams@gnu.org>
2002-04-29 21:07:32 +0000

    <version>: Include removed.

--- ping/ping_address.c
+++ ping/ping_address.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001 Free Software Foundation, Inc.
+/* Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 
    This file is part of GNU Inetutils.
 
@@ -46,7 +46,6 @@
 #include <errno.h>
 
 #include "getopt.h"
-#include "version.h"
 #include <icmp.h>
 #include <ping.h>
 #include <ping_impl.h>
