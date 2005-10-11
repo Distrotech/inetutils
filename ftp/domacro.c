38ac5a56f8045f594c4fe63c65e35e6c50f0a444
Sergey Poznyakoff <gray@Mirddin.farlep.net>
2005-10-11 11:16:40 +0000

    Remove leftover BSD-isms: err,errx,warn,warnx, cast to (void) before function calls. Use GNU error() uniformly Use do while loop instead of goto.

--- ftp/domacro.c
+++ ftp/domacro.c
@@ -28,7 +28,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)domacro.c	8.3 (Berkeley) 4/2/94";
+static char     sccsid[] = "@(#)domacro.c	8.3 (Berkeley) 4/2/94";
 #endif /* not lint */
 
 #ifdef HAVE_CONFIG_H
@@ -43,109 +43,119 @@ static char sccsid[] = "@(#)domacro.c	8.3 (Berkeley) 4/2/94";
 #include "ftp_var.h"
 
 void
-domacro(argc, argv)
-	int argc;
-	char *argv[];
+domacro (int argc, char *argv[])
 {
-	int i, j, count = 2, loopflg = 0;
-	char *cp1, *cp2, line2[200];
-	struct cmd *c;
+  int             i, j, count = 2, loopflg = 0;
+  char           *cp1, *cp2, line2[200];
+  struct cmd     *c;
 
-	if (argc < 2 && !another(&argc, &argv, "macro name")) {
-		printf("Usage: %s macro_name.\n", argv[0]);
-		code = -1;
-		return;
+  if (argc < 2 && !another (&argc, &argv, "macro name"))
+    {
+      printf ("Usage: %s macro_name.\n", argv[0]);
+      code = -1;
+      return;
+    }
+  for (i = 0; i < macnum; ++i)
+    {
+      if (!strncmp (argv[1], macros[i].mac_name, 9))
+	{
+	  break;
 	}
-	for (i = 0; i < macnum; ++i) {
-		if (!strncmp(argv[1], macros[i].mac_name, 9)) {
-			break;
-		}
-	}
-	if (i == macnum) {
-		printf("'%s' macro not found.\n", argv[1]);
-		code = -1;
-		return;
-	}
-	(void) strcpy(line2, line);
-TOP:
-	cp1 = macros[i].mac_start;
-	while (cp1 != macros[i].mac_end) {
-		while (isspace(*cp1)) {
-			cp1++;
-		}
-		cp2 = line;
-		while (*cp1 != '\0') {
-		      switch(*cp1) {
-		   	    case '\\':
-				 *cp2++ = *++cp1;
-				 break;
-			    case '$':
-				 if (isdigit(*(cp1+1))) {
-				    j = 0;
-				    while (isdigit(*++cp1)) {
-					  j = 10*j +  *cp1 - '0';
-				    }
-				    cp1--;
-				    if (argc - 2 >= j) {
-					(void) strcpy(cp2, argv[j+1]);
-					cp2 += strlen(argv[j+1]);
-				    }
-				    break;
-				 }
-				 if (*(cp1+1) == 'i') {
-					loopflg = 1;
-					cp1++;
-					if (count < argc) {
-					   (void) strcpy(cp2, argv[count]);
-					   cp2 += strlen(argv[count]);
-					}
-					break;
-				}
-				/* intentional drop through */
-			    default:
-				*cp2++ = *cp1;
-				break;
-		      }
-		      if (*cp1 != '\0') {
-			 cp1++;
-		      }
-		}
-		*cp2 = '\0';
-		makeargv();
-		if (margv[0] == NULL)
-			return;
-		c = getcmd(margv[0]);
-		
-		if (c == (struct cmd *)-1) {
-			printf("?Ambiguous command\n");
-			code = -1;
-		}
-		else if (c == 0) {
-			printf("?Invalid command\n");
-			code = -1;
-		}
-		else if (c->c_conn && !connected) {
-			printf("Not connected.\n");
-			code = -1;
-		}
-		else {
-			if (verbose) {
-				printf("%s\n",line);
+    }
+  if (i == macnum)
+    {
+      printf ("'%s' macro not found.\n", argv[1]);
+      code = -1;
+      return;
+    }
+  strcpy (line2, line);
+
+  do
+    {
+      cp1 = macros[i].mac_start;
+      while (cp1 != macros[i].mac_end)
+	{
+	  while (isspace (*cp1))
+	    {
+	      cp1++;
+	    }
+	  cp2 = line;
+	  while (*cp1 != '\0')
+	    {
+	      switch (*cp1)
+		{
+		case '\\':
+		  *cp2++ = *++cp1;
+		  break;
+		case '$':
+		  if (isdigit (*(cp1 + 1)))
+		    {
+		      j = 0;
+		      while (isdigit (*++cp1))
+			j = 10 * j + *cp1 - '0';
+		      cp1--;
+		      if (argc - 2 >= j)
+			{
+			  strcpy (cp2, argv[j + 1]);
+			  cp2 += strlen (argv[j + 1]);
 			}
-			(*c->c_handler)(margc, margv);
-			if (bell && c->c_bell) {
-				(void) putchar('\007');
+		      break;
+		    }
+		  if (*(cp1 + 1) == 'i')
+		    {
+		      loopflg = 1;
+		      cp1++;
+		      if (count < argc)
+			{
+			  strcpy (cp2, argv[count]);
+			  cp2 += strlen (argv[count]);
 			}
-			(void) strcpy(line, line2);
-			makeargv();
-			argc = margc;
-			argv = margv;
+		      break;
+		    }
+		  /* intentional drop through */
+		default:
+		  *cp2++ = *cp1;
+		  break;
 		}
-		if (cp1 != macros[i].mac_end) {
-			cp1++;
-		}
+	      if (*cp1 != '\0')
+		cp1++;
+	    }
+	  *cp2 = '\0';
+	  makeargv ();
+	  if (margv[0] == NULL)
+	    return;
+	  c = getcmd (margv[0]);
+	  
+	  if (c == (struct cmd *) -1)
+	    {
+	      printf ("?Ambiguous command\n");
+	      code = -1;
+	    }
+	  else if (c == 0)
+	    {
+	      printf ("?Invalid command\n");
+	      code = -1;
+	    }
+	  else if (c->c_conn && !connected)
+	    {
+	      printf ("Not connected.\n");
+	      code = -1;
+	    }
+	  else
+	    {
+	      if (verbose)
+		printf ("%s\n", line);
+	      (*c->c_handler) (margc, margv);
+	      if (bell && c->c_bell)
+		putchar ('\007');
+	      strcpy (line, line2);
+	      makeargv ();
+	      argc = margc;
+	      argv = margv;
+	    }
+	  if (cp1 != macros[i].mac_end)
+	    cp1++;
 	}
-	if (loopflg && ++count < argc) {
-		goto TOP;
-	}
+    }
+  while (loopflg && ++count < argc);
 }
