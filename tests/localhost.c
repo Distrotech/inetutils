664da77a47b81896db58f3156ec8bb0a0a0d42ad
Simon Josefsson <simon@josefsson.org>
2007-04-12 11:24:43 +0000

    Add initial tests/ for self-tests.

---
+++ tests/localhost.c
@@ -0,0 +1,33 @@
+/* Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This file is part of GNU Inetutils.
+
+   GNU Inetutils is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   GNU Inetutils is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Inetutils; see the file COPYING.  If not, write
+   to the Free Software Foundation, Inc., 51 Franklin Street,
+   Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#include "libinetutils.h"
+
+int
+main (void)
+{
+  const char *p = localhost ();
+
+  if (!p)
+    return 1;
+
+  printf ("localhost: %s\n", p);
+
+  return 0;
+}
