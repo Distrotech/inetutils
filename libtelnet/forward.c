ea2cdf86bfecae2b1c81fb07c3a43aedd4887ac3
Simon Josefsson <simon@josefsson.org>
2009-06-10 09:43:21 +0000

    Fix license and copyright statement, based on mailing list discussions.

--- libtelnet/forward.c
+++ libtelnet/forward.c
@@ -1,7 +1,23 @@
 /*
- *    $Source$
- *    $Id$
- */
+  Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005
+                2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+
+  This file is part of GNU InetUtils.
+
+  GNU InetUtils is free software: you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  GNU InetUtils is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
 
 /*-
  * Copyright (c) 1993
--- libtelnet/shishi.c
+++ libtelnet/shishi.c
@@ -1,21 +1,23 @@
-/* Copyright (C) 2003, 2007, 2008 Free Software Foundation, Inc.
+/*
+  Copyright (C) 2003, 2004, 2005, 2006, 2007,
+                2008, 2009 Free Software Foundation, Inc.
 
-This file is part of Shishi / GNU Inetutils.
+  This file is part of GNU InetUtils.
 
-Shishi is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
+  GNU InetUtils is free software: you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
 
-Shishi is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  GNU InetUtils is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Shishi; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA. */
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
 
 /* Written by Simon Josefsson and Nicolas Pouvesle, based on
    kerberos5.c from GNU InetUtils. */
