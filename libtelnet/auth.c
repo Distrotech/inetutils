e9eaaf6158b8e5d3b0d7b3b33e5413e36efaa52c
Simon Josefsson <simon@josefsson.org>
2009-06-15 09:24:58 +0000

    libtelnet/*: Change use of 'InetUtils' to 'Inetutils'.

--- libtelnet/auth.c
+++ libtelnet/auth.c
@@ -2,14 +2,14 @@
   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005
                 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
 
-  This file is part of GNU InetUtils.
+  This file is part of GNU Inetutils.
 
-  GNU InetUtils is free software: you can redistribute it and/or
+  GNU Inetutils is free software: you can redistribute it and/or
   modify it under the terms of the GNU General Public License as
   published by the Free Software Foundation, either version 3 of the
   License, or (at your option) any later version.
 
-  GNU InetUtils is distributed in the hope that it will be useful, but
+  GNU Inetutils is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   General Public License for more details.
--- libtelnet/shishi.c
+++ libtelnet/shishi.c
@@ -2,14 +2,14 @@
   Copyright (C) 2003, 2004, 2005, 2006, 2007,
                 2008, 2009 Free Software Foundation, Inc.
 
-  This file is part of GNU InetUtils.
+  This file is part of GNU Inetutils.
 
-  GNU InetUtils is free software: you can redistribute it and/or
+  GNU Inetutils is free software: you can redistribute it and/or
   modify it under the terms of the GNU General Public License as
   published by the Free Software Foundation, either version 3 of the
   License, or (at your option) any later version.
 
-  GNU InetUtils is distributed in the hope that it will be useful, but
+  GNU Inetutils is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   General Public License for more details.
@@ -20,7 +20,7 @@
 */
 
 /* Written by Simon Josefsson and Nicolas Pouvesle, based on
-   kerberos5.c from GNU InetUtils. */
+   kerberos5.c from GNU Inetutils. */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
