fa90ce485a5ff1193473eafd2232716ba50c5cbe
Simon Josefsson <simon@josefsson.org>
2009-11-16 12:05:31 +0100

    Fix ping/traceroute self-tests.

--- tests/ping-localhost.sh
+++ tests/ping-localhost.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
+# Copyright (C) 2009 Free Software Foundation, Inc.
+#
+# This file is part of GNU Inetutils.
+#
+# GNU Inetutils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# GNU Inetutils is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see `http://www.gnu.org/licenses/'.
+
+PING=${PING:-../ping/ping$EXEEXT}
+
 if [ $VERBOSE ]; then
     set -x
-    ping --version
+    $PING --version
 fi
 
 if [ `id -u` != 0 ]; then
@@ -10,6 +29,6 @@ if [ `id -u` != 0 ]; then
     exit 77
 fi
 
-ping -c 1 127.0.0.1; errno=$?
+$PING -c 1 127.0.0.1; errno=$?
 
 exit $errno
