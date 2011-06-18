e6cdeb0a4a304079893b11b63a353ba1f7fc9bec
Alfred M. Szmidt <ams@gnu.org>
2011-06-18 14:27:04 +0200

    libinetutils/libinetutils.h: Don't include <config.h>; instead produce an error if we haven't included it.

--- libinetutils/libinetutils.h
+++ libinetutils/libinetutils.h
@@ -17,7 +17,9 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see `http://www.gnu.org/licenses/'. */
 
-#include <config.h>
+#ifndef HAVE_CONFIG_H
+#error "<config.h> has not been included; please included it"
+#endif
 
 #include "argp-version-etc.h"
 
