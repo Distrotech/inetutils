0aeea0cb612ca487908380ce37dcb96b0a0afd59
Simon Josefsson <simon@josefsson.org>
2011-11-19 11:39:41 +0100

    tests: Added self-tests of ftp and ftpd (using inetd).
    * tests/Makefile.am (dist_check_SCRIPTS): Add ftp-localhost.sh.
    * tests/ftp-localhost.sh: New file.

---
+++ tests/ftp-localhost.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Copyright (C) 2011 Free Software Foundation, Inc.
+#
+# This file is part of GNU Inetutils.
+#
+# GNU Inetutils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# GNU Inetutils is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see `http://www.gnu.org/licenses/'.
+
+# Written by Simon Josefsson
+
+set -e
+
+FTP=${FTP:-../ftp/ftp$EXEEXT}
+FTPD=${FTPD:-../ftpd/ftpd$EXEEXT}
+INETD=${INETD:-../src/inetd$EXEEXT}
+TARGET=${TARGET:-127.0.0.1}
+
+if [ $VERBOSE ]; then
+    set -x
+    $FTP --version | head -1
+    $FTPD --version | head -1
+    $INETD --version | head -1
+fi
+
+if [ `id -u` != 0 ]; then
+    echo "ftpd needs to run as root"
+    exit 77
+fi
+
+if ! id -u ftp > /dev/null; then
+    echo "anonymous ftpd needs a 'ftp' user"
+    exit 77
+fi
+
+# Note that inetd changes directory to / when --debug is not given so
+# all paths must be absolute for things to work.
+
+TMPDIR=`mktemp --tmpdir=$PWD -d`
+trap 'rm -rf "$TMPDIR"' 0 1 2 3 15
+
+echo "4711 stream tcp nowait root $PWD/$FTPD ftpd -A -l" > $TMPDIR/inetd.conf
+echo "machine 127.0.0.1 login ftp password foobar" > $TMPDIR/.netrc
+chmod 600 $TMPDIR/.netrc
+
+$INETD --pidfile=$TMPDIR/inetd.pid $TMPDIR/inetd.conf
+
+# Wait for inetd to write pid and open socket
+sleep 2
+
+echo "rstatus\ndir\n" \
+    | HOME=$TMPDIR $FTP $TARGET 4711 -v -p -t | tee $TMPDIR/ftp.stdout
+errno=$?
+if [ $errno != 0 ]; then
+    echo running ftp failed? errno $errno
+    exit 77
+fi
+
+if [ $errno != 0 ]; then
+    echo running ftp failed? errno $errno
+    exit 77
+fi
+
+if ! grep 'FTP server status' $TMPDIR/ftp.stdout; then
+    echo cannot find expected output from ftp client?
+    exit 1
+fi
+
+inetd_pid=`cat $TMPDIR/inetd.pid`
+kill -9 $inetd_pid
+
+exit 0
