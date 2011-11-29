77d5ea7a6fcbf8cb1789b60522a2cecab36c73cd
Mats Erik Andersson <gnu@gisladisker.se>
2011-11-29 13:11:55 +0100

    make_ip_del.pl: Arithmetic on 64-bit systems.

--- whois/make_ip_del.pl
+++ whois/make_ip_del.pl
@@ -29,7 +29,7 @@ while (<>) {
 	my $m=$2; my $s=$3;
 	my ($i1, $i2, $i3, $i4)=split(/\./, $1);
 	print "{ ".(($i1<<24)+($i2<<16)+($i3<<8)+$i4)."UL, ".
-		(~(0xffffffff>>$m))."UL, \"";
+		(0xffffffff^(0xffffffff>>$m))."UL, \"";
 	if ($s =~ /\./) {
 		print "$s";
 	} else {
